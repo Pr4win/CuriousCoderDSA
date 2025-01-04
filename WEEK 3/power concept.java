ae1192fe933faf7a07b1fca663a8b973b8275c0f
Pr4win <127928960+Pr4win@users.noreply.github.com>
2025-01-04 20:47:10 +0530

    Create power concept.java

---
+++ WEEK 3/power concept.java
@@ -0,0 +1,12 @@
+//week3 power concept
+class Main {
+    public static void main(String[] args) {
+       int A=7;
+       int B=3;
+       int result = A;
+        for(int i=1; i<B; i++){ 
+            result = A*result;
+        }
+        System.out.println(result);
+    }
+}
