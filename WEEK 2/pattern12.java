743e14919587e35013e17b6102be32b586af9322
Pr4win <127928960+Pr4win@users.noreply.github.com>
2025-01-04 08:51:19 +0530

    Create pattern12.java

---
+++ WEEK 2/pattern12.java
@@ -0,0 +1,24 @@
+//pattern 12(combination of pattern10 and pattern11)
+class Main {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=n-1; i++){
+            for(int j=1; j<=n-i; j++){
+            System.out.print(" "+" ");
+            }
+            for(int j=1; j<=i*2-1; j++){
+            System.out.print("*"+" ");
+            }
+        System.out.println("");
+        }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i-1; j++){
+            System.out.print(" "+" ");
+            }
+            for(int j=1; j<=(2*n)-(2*i-1); j++){
+            System.out.print("*"+" ");
+            }
+        System.out.println("");
+        }
+    }
+}
