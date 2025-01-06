3b4e5e978dcdbb5475773ed6f77042ab12266b6c
Pr4win <127928960+Pr4win@users.noreply.github.com>
2025-01-06 12:39:22 +0530

    Update sum of first n^3 terms.java

--- WEEK 4/sum of first n^3 terms.java
+++ WEEK 4/sum of first n^3 terms.java
@@ -1,5 +1,5 @@
 //Sum of first n^3 terms.
-/* Given an integer n, calculate the sum of series 13 + 23 + 33 + 43 + … till n-th term.
+/* Given an integer n, calculate the sum of series 1^3 + 2^3 + 3^3 + 4^3 + … till n-th term.
 Input: n = 5,Output: 225
 Explanation: 1^3 + 2^3 + 3^3 + 4^3 + 5^3 = 225   */
 import java.util.Scanner;
