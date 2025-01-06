79d0b6d7f3fee58bc3d659f79e5a14efbaa39538
Pr4win <127928960+Pr4win@users.noreply.github.com>
2025-01-06 18:59:12 +0530

    Create iterating array elements.java

---
+++ WEEK 4/iterating array elements.java
@@ -0,0 +1,10 @@
+// Iterating array elements
+class Main{
+    public static void main(String[] args){
+        int a[]={345,387,67,988,876};
+        int n=5;
+        for(int i=0;i<n;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
