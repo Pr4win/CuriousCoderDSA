c048ed7b1475cbbfe9760af7bd22f259c4b1f0de
Pr4win <127928960+Pr4win@users.noreply.github.com>
2025-01-06 12:34:25 +0530

    Create sum of first n^3 terms.java

---
+++ WEEK 4/sum of first n^3 terms.java
@@ -0,0 +1,17 @@
+//Sum of first n^3 terms.
+/* Given an integer n, calculate the sum of series 13 + 23 + 33 + 43 + … till n-th term.
+Input: n = 5,Output: 225
+Explanation: 1^3 + 2^3 + 3^3 + 4^3 + 5^3 = 225   */
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+          System.out.println("ENTER N"); 
+          Scanner sc = new Scanner(System.in);
+          int n = sc.nextInt();
+          int sum=0;
+          for(int i=1;i<=n;i++){
+            sum=sum+i*i*i;
+          }
+          System.out.println(sum);    
+    }
+}
