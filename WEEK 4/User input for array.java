9b054fa5bddb9b42b061bbe638ecf75e3a60f946
Pr4win <127928960+Pr4win@users.noreply.github.com>
2025-01-06 19:08:52 +0530

    Create User input for array.java

---
+++ WEEK 4/User input for array.java
@@ -0,0 +1,18 @@
+// User input for array
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        System.out.println("ENTER ARRAY SIZE");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        System.out.println("ENTER ARRAY ELEMENTS");
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        System.out.println("INPUT OVER");
+        for(int i=0;i<n;i++){
+            System.out.println(a[i]);
+        }
+    }
+}
