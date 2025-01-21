40dde48179b4f1c9cec0acb5db1c798452a4d0e8
Pr4win <127928960+Pr4win@users.noreply.github.com>
2025-01-21 20:06:08 +0530

    Update remove duplicates.java

--- WEEK 6/ARRAY PROBLEMS/remove duplicates.java
+++ WEEK 6/ARRAY PROBLEMS/remove duplicates.java
@@ -3,19 +3,16 @@ class Main {
     public static void main(String[] args) {
         int a []={2,4,4,4,4,6,6,7,8,9,9,10,10};
         int n=a.length;
-        int j=0,i=0;
-        while(i<n-1){
-            if(a[i]<a[i+1]){
+        int j=0;
+        for(int i=0; i<n-1; i++){
+            if(a[i]!=a[i+1]){
                 a[j]=a[i];
                 j++;
-                i++;
-            }else{
-                i++;
             }
         }
         a[j]=a[n-1];
         j++;
-        for(i=0; i<j; i++){
+        for(int i=0; i<j; i++){
             System.out.print(a[i]+" ");
         }
     }
