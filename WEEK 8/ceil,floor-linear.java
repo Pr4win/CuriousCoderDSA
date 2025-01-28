df42f3537e5a32946bd7b487d5adf1152237c92b
Pr4win <127928960+Pr4win@users.noreply.github.com>
2025-01-28 11:30:30 +0530

    Update ceil,floor-linear.java

--- WEEK 8/ceil,floor-linear.java
+++ WEEK 8/ceil,floor-linear.java
@@ -18,7 +18,7 @@ class Main {
         return -1;
     }
     static int ceil(int[] a,int n,int x){
-        for(int i=0; i<=n; i++){
+        for(int i=0; i<n; i++){
             if(a[i]>=x){
                 return a[i];
             }
